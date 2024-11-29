6774c0b59758051c2522069fff8648849d307bda
Uneto47 <netovasconcelos02@gmail.com>
2024-11-28 21:09:18 -0300

    feat: local storage for persistent data

--- src/store/favorites.js
+++ src/store/favorites.js
@@ -2,16 +2,21 @@ import { defineStore } from 'pinia';
 
 export const useFavoritesStore = defineStore('favorites', {
   state: () => ({
-    favorites: [],
+    favorites: JSON.parse(localStorage.getItem('favorites')) || [],
   }),
   actions: {
     addFavorite(image) {
       if (!this.favorites.find((fav) => fav.id === image.id)) {
         this.favorites.push(image);
+        this.saveFavoritesToLocalStorage();
       }
     },
     removeFavorite(imageId) {
       this.favorites = this.favorites.filter((fav) => fav.id !== imageId);
+      this.saveFavoritesToLocalStorage();
+    },
+    saveFavoritesToLocalStorage() {
+      localStorage.setItem('favorites', JSON.stringify(this.favorites));
     },
   },
   getters: {
